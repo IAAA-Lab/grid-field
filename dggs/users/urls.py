1caf3680fc9f054d3a52b156befab6604c3cad6c
Muhammad Umer <m.umer_ck@yahoo.com>
2021-06-03 17:09:21 +0200

    logout

--- dggs/users/urls.py
+++ dggs/users/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from . import views
-
 urlpatterns = [
     url(r'^register/$', views.register_request, name="register"),
-    url(r'^login/$', views.login_request, name="login")
+    url(r'^login/$', views.login_request, name="login"),
+    url(r'^logout/$', views.logout_request, name="logout"),
 ]
